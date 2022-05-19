59bc19993fb9e5853ac1fd8aa47304e1c7eddd17
Benj <benjjensen@live.com>
2022-05-19 12:12:09 -0400

    Adjusted minor things

--- src/MissionSim/Simulator/README.md
+++ src/MissionSim/Simulator/README.md
@@ -1,2 +1,6 @@
 # SIMULATOR
 This folder contains a simulator used to propogate state dynamics and generate the measurements that the various filters use. Because this file essentially plays the role of the environment, it will not be used in the actual flight and is merely for generating data to work with. 
+
+## Simulator Module 
+This module exports 'rk4', the primary function for updating state, and 'generate_measurements', which takes in  the state and generates 
+both idealized and noisy measurements.
--- src/MissionSim/Simulator/Simulator.jl
+++ src/MissionSim/Simulator/Simulator.jl
@@ -2,26 +2,23 @@
 
 """ To Do:
 
-    - Separate Dynamics, Measurements 
-    - Comment, test, and speed up each 
     - Update README
-    - Dynamics:
-        - Add in drag that is dependent on orientation?
-        - Get accurate gyro bias values
-        - See if eclipse is updated (update here and in measurement)
+    - Dynamics: see TODO in dynamics.jl 
+    - Measurements: see TODO in measurements.jl
 """
 
 module Simulator 
 
-include("../CustomStructs.jl"); using .CustomStructs 
-include("../quaternions.jl")
 using LinearAlgebra, StaticArrays 
 using EarthAlbedo, SatelliteDynamics 
-using Distributions
+using Distributions, Plots
 
+include("../CustomStructs.jl"); using .CustomStructs 
+include("../quaternions.jl")
 include("dynamics.jl")
 include("measurements.jl")
 include("../mag_field.jl")
+
 
 export rk4 
 export generate_measurements
