ce9a17173aa94fbc2995d62160d70dd331d46c12
Benj <benjjensen@live.com>
2022-07-14 13:01:12 -0400

    Adjusted Noise values
    Also separated the iterated and non-iterated

--- src/Estimator/mekf.jl
+++ src/Estimator/mekf.jl
@@ -7,6 +7,9 @@
 
  - Read up on IMU stuff to get a fill for the noise W and V (I have no idea..) 
  - Read up on setting initial covariances too
+
+ - When in eclipse, dont use I and don't use the Ĩ for z or H either (adjust sizes)
+ - Easy functionality for testing (self-consistency, innovation `z` is unbiased noise, etc... )
 """
 
 """
@@ -26,20 +29,20 @@ struct MEKF_DATA{T}
         new{T}(Wchol, Vchol)
     end
 
-    function MEKF_DATA(; N = 6, σ_mag = deg2rad(3.0), σ_cur = 0.1,
-                            σs = 1e-4, σζ = deg2rad(0.01),  
-                            σC = 1e-4, σα = deg2rad(0.01), σϵ = deg2rad(0.01),
+    function MEKF_DATA(; N = 6, σ_mag = deg2rad(20), σ_cur = 0.05, # σ_mag = deg2rad(3.0), σ_cur = 0.1,
+                            σs = 1e-5, σζ = deg2rad(0.01), σβₘ = 1e-5,  
+                            σC = 1e-5, σα = deg2rad(0.01), σϵ = deg2rad(0.01),
                             gyro_bias_instability = 0.8,   # in deg/hour  
                             angle_random_walk     = 0.06)  # in deg/sqrt(hour)
 
         """ Generates a new MEKF_DATA by generating the W and V matrices with the provided values """
 
         ## Process Noise:
-        σ_gyro = deg2rad(gyro_bias_instability) / 3600.0  # Convert (deg/hour) to (rad/sec)
-        σ_bias = deg2rad(angle_random_walk) / 60.0        # Convert (deg/sqrt(hour)) to ( rad/sqrt(s) )
+        σ_gyro = 5e-4 #deg2rad(gyro_bias_instability) / 3600.0  # Convert (deg/hour) to (rad/sec)
+        σ_bias = 3.14e-5 # deg2rad(angle_random_walk) / 60.0        # Convert (deg/sqrt(hour)) to ( rad/sqrt(s) )
         
         # Just using the same bias noise for both 
-        W = Diagonal( [σ_gyro * ones(3); σ_bias * ones(3); σs * ones(3); σζ * ones(3); 10 * σ_bias * ones(3); σC * ones(N); σα * ones(N); σϵ * ones(N)].^2 )
+        W = Diagonal( [σ_gyro * ones(3); σ_bias * ones(3); σs * ones(3); σζ * ones(3); σβₘ * ones(3); σC * ones(N); σα * ones(N); σϵ * ones(N)].^2 )
         Wchol = chol(Matrix(W)) 
         
         ## Measurement Noise:
@@ -50,6 +53,9 @@ struct MEKF_DATA{T}
     end
 
 end
+
+# @warn "Eliminated reset cov!"
+@warn "Using iter sqrt MEKF, and no reset cov!"
 
 # May have problems due to re-diagonalizing
 """
@@ -65,6 +71,7 @@ end
 function reset_cov!(sat::SATELLITE{N, T}; reset_calibration = false, σϕ = deg2rad(10), σβ = deg2rad(10), 
                         σs = 0.3, σζ = deg2rad(9), σβₘ = deg2rad(12), σC = 0.3, σα = deg2rad(9.0), σϵ = deg2rad(9)) where {N, T}
     
+    return nothing
     # # Update Covariances 
     # Reset q, increase β, pass C, α, and ϵ
     Σϕ = diagm( σϕ * ones(3) )  # Reset (remember this is Cholesky so no σ²)
@@ -74,18 +81,31 @@ function reset_cov!(sat::SATELLITE{N, T}; reset_calibration = false, σϕ = deg2
     sat.covariance[4:6, 4:6] .= Σβ
 
     if reset_calibration 
-        sat.covariance[7:9, 7:9] .= diagm(σs * ones(3))
-        sat.covariance[10:12, 10:12] .= diagm(σζ * ones(3))
-        sat.covariance[13:15, 13:15] .= diagm(σβₘ * ones(3))
+        # sat.covariance[7:9, 7:9] .= diagm(σs * ones(3))
+        # sat.covariance[10:12, 10:12] .= diagm(σζ * ones(3))
+        # sat.covariance[13:15, 13:15] .= diagm(σβₘ * ones(3))
+
+        # i₀ = 16; i₁ = i₀ + N - 1
+        # sat.covariance[i₀:i₁, i₀:i₁] .= diagm(σC * ones(N))
+
+        # i₀ = i₁ + 1; i₁ = i₀ + N - 1
+        # sat.covariance[i₀:i₁, i₀:i₁] .= diagm(σα * ones(N))
+
+        # i₀ = i₁ + 1; i₁ = i₀ + N - 1
+        # sat.covariance[i₀:i₁, i₀:i₁] .= diagm(σϵ * ones(N))
+
+        sat.covariance[7:9, 7:9] *= 10 
+        sat.covariance[10:12, 10:12] *= 10
+        sat.covariance[13:15, 13:15] *= 10
 
         i₀ = 16; i₁ = i₀ + N - 1
-        sat.covariance[i₀:i₁, i₀:i₁] .= diagm(σC * ones(N))
+        sat.covariance[i₀:i₁, i₀:i₁] *= 10
 
         i₀ = i₁ + 1; i₁ = i₀ + N - 1
-        sat.covariance[i₀:i₁, i₀:i₁] .= diagm(σα * ones(N))
+        sat.covariance[i₀:i₁, i₀:i₁] *= 10
 
         i₀ = i₁ + 1; i₁ = i₀ + N - 1
-        sat.covariance[i₀:i₁, i₀:i₁] .= diagm(σϵ * ones(N))
+        sat.covariance[i₀:i₁, i₀:i₁] *= 10
     end
 
     return Nothing
@@ -126,10 +146,11 @@ function estimate(sat::SATELLITE{N, T}, sens::SENSORS{N, T}, noise::MEKF_DATA{T}
     # Pass into square-root MEKF 
     Pchol = (calibrate) ? sat.covariance : sat.covariance[1:6, 1:6]
     Wchol = (calibrate) ? noise.Wchol    : UpperTriangular(noise.Wchol[1:6, 1:6])
-    x⁺, mag⁺, diode⁺, Pchol⁺ = sqrt_mekf(sat.state, sat.diodes, sat.magnetometer, Pchol, alb, sens.gyro, Bᴵ_est, sᴵ_est,
-                             sens.magnetometer, sens.diodes, sens.pos, dt, Wchol, noise.Vchol; 
-                             use_albedo = use_albedo, calibrate = calibrate, E_am₀ = E_am₀)
 
+    # Remember to change reports for self consistency too
+    x⁺, mag⁺, diode⁺, Pchol⁺ = iter_sqrt_mekf(sat.state, sat.diodes, sat.magnetometer, Pchol, alb, sens.gyro, Bᴵ_est, sᴵ_est,
+                                         sens.magnetometer, sens.diodes, sens.pos, dt, Wchol, noise.Vchol; 
+                                         use_albedo = use_albedo, calibrate = calibrate, E_am₀ = E_am₀)
 
     # Process result and update sat (by making a new copy)
     sat⁺ = SATELLITE(deepcopy(sat.J), mag⁺, diode⁺, x⁺, copy(sat.covariance) )
@@ -159,29 +180,40 @@ function sqrt_mekf(x::SAT_STATE{T}, diodes::DIODES{N, T}, mag::MAGNETOMETER{T}, 
     x_pred, A = prediction(x, ω, dt, N; calibrate = calibrate)
     Pchol_pred = qrᵣ([Pchol * A'; W])  # Update covariance (as Cholesky)
 
-    # # Measurement (do NOT normalize this, or at least not the current one)
-    # B̃ᴮ_exp, H_mag = mag_measurement(x_pred, mag, Bᴵ, N; calibrate = calibrate)
+    # Measurement (do NOT normalize this, or at least not the current one)
+    B̃ᴮ_exp, H_mag = mag_measurement(x_pred, mag, Bᴵ, N; calibrate = calibrate)
 
-    # #   If in eclipse, dont use I 
-    # I_exp,  H_cur = (norm(Ĩ) > 0.05) ?   current_measurement(x_pred, diodes, sᴵ, pos, alb; E_am₀ = E_am₀,
-    #                                     use_albedo = use_albedo, calibrate = calibrate)  :
-    #                 (calibrate) ? (zeros(N), zeros(N, 15 + 3 * N)) : (zeros(N), zeros(N, 6))                        
+    #   If in eclipse, dont use I 
+    I_exp,  H_cur = (norm(Ĩ) > 0.05) ?  current_measurement(x_pred, diodes, sᴵ, pos, alb; E_am₀ = E_am₀,
+                                                            use_albedo = use_albedo, calibrate = calibrate)  :
+                    (calibrate)      ? (zeros(N), zeros(N, 15 + 3 * N)) : (zeros(N), zeros(N, 6))                        
 
-    # # Innovation 
-    # z = [B̃ᴮ - B̃ᴮ_exp; Ĩ - I_exp]
-    # H = [H_mag; H_cur]
-    # Pchol_yy = qrᵣ([Pchol_pred * H'; V])
-    # L = (((Pchol_pred' * Pchol_pred * H') / Pchol_yy) / Pchol_yy')
-    # x⁺, mag⁺, diodes⁺ = update(x_pred, mag, diodes, L, z; calibrate = calibrate) 
+    # Innovation 
+    z = [B̃ᴮ - B̃ᴮ_exp; Ĩ - I_exp]
+    H = [H_mag; H_cur]
+    Pchol_yy = qrᵣ([Pchol_pred * H'; V])
+    L = (((Pchol_pred' * Pchol_pred * H') / Pchol_yy) / Pchol_yy')
+    x⁺, mag⁺, diodes⁺ = update(x_pred, mag, diodes, L, z; calibrate = calibrate) 
+
+    # Update 
+    Pchol⁺ = qrᵣ([ Pchol_pred * (I - L * H)'; V * L']); 
+
+    return x⁺, mag⁺, diodes⁺, Pchol⁺
+end
+
+function iter_sqrt_mekf(x::SAT_STATE{T}, diodes::DIODES{N, T}, mag::MAGNETOMETER{T}, Pchol::Matrix{T}, alb::ALBEDO, 
+                    ω::SVector{3, T}, Bᴵ::SVector{3, T}, sᴵ::SVector{3, T}, B̃ᴮ::SVector{3, T}, 
+                    Ĩ::SVector{N, T}, pos::SVector{3, T}, dt::T, W::UpperTriangular, V::UpperTriangular; 
+                    E_am₀ = 1366.9, use_albedo = true, calibrate = true) where {N, T}
+
+
+    ### ITERATION 1 ###
+    # Prediction 
+    x_pred, A = prediction(x, ω, dt, N; calibrate = calibrate)
+    Pchol_pred = qrᵣ([Pchol * A'; W])  # Update covariance (as Cholesky)
 
     x⁺, mag⁺, diodes⁺ = deepcopy(x_pred), deepcopy(mag), deepcopy(diodes)
     L, H = 0.0, 0.0
-
-    # x⁺ = deepcopy(x_pred)
-    # x_prior = deepcopy(x_pred)
-    # mag_prior = deepcopy(mag)
-    # diodes_prior = deepcopy(diodes)
-    # L, H = 0, 0, 
 
     # Kalman Gain (how much we trust the measurement over the dynamics)
     for _ = 1:3
@@ -190,11 +222,11 @@ function sqrt_mekf(x::SAT_STATE{T}, diodes::DIODES{N, T}, mag::MAGNETOMETER{T}, 
 
         #   If in eclipse, dont use I 
         I_exp,  H_cur = (norm(Ĩ) > 0.05) ?  current_measurement(x⁺, diodes⁺, sᴵ, pos, alb; E_am₀ = E_am₀,
-                                            use_albedo = use_albedo, calibrate = calibrate)  :
-                        (calibrate) ? (zeros(N), zeros(N, 15 + 3 * N)) : (zeros(N), zeros(N, 6))                        
+                                    use_albedo = use_albedo, calibrate = calibrate)  :
+                (calibrate) ? (zeros(N), zeros(N, 15 + 3 * N)) : (zeros(N), zeros(N, 6))                        
 
         # Innovation 
-        z = [B̃ᴮ - B̃ᴮ_exp; Ĩ - I_exp]
+        z = [B̃ᴮ - B̃ᴮ_exp; Ĩ - I_exp]     # maybe?
         H = [H_mag; H_cur]
 
         # Update Covariance 
@@ -202,14 +234,85 @@ function sqrt_mekf(x::SAT_STATE{T}, diodes::DIODES{N, T}, mag::MAGNETOMETER{T}, 
         L = (((Pchol_pred' * Pchol_pred * H') / Pchol_yy) / Pchol_yy')
 
         x⁺, mag⁺, diodes⁺ = update(x_pred, mag, diodes, L, z; calibrate = calibrate)
+
     end
 
     # Update 
     Pchol⁺ = qrᵣ([ Pchol_pred * (I - L * H)'; V * L']); 
 
+    return x⁺, mag⁺, diodes⁺, Pchol⁺
+end
+
+function mekf(x::SAT_STATE{T}, diodes::DIODES{N, T}, mag::MAGNETOMETER{T}, Pchol::Matrix{T}, alb::ALBEDO, 
+    ω::SVector{3, T}, Bᴵ::SVector{3, T}, sᴵ::SVector{3, T}, B̃ᴮ::SVector{3, T}, 
+    Ĩ::SVector{N, T}, pos::SVector{3, T}, dt::T, W::UpperTriangular, V::UpperTriangular; 
+    E_am₀ = 1366.9, use_albedo = true, calibrate = true) where {N, T}
+
+    P_prev = Pchol' * Pchol 
+    Q = W' * W 
+    R = V' * V
+
+    ### ITERATION 1 ###
+    # Prediction 
+    x_pred, A = prediction(x, ω, dt, N; calibrate = calibrate)
+    P_pred = A * P_prev * A' + Q 
+    Pchol_pred = qrᵣ([Pchol * A'; W])  # Update covariance (as Cholesky)
+
+    # # Measurement (do NOT normalize this, or at least not the current one)
+    B̃ᴮ_exp, H_mag = mag_measurement(x_pred, mag, Bᴵ, N; calibrate = calibrate)
+
+    #   If in eclipse, dont use I 
+    I_exp,  H_cur = (norm(Ĩ) > 0.05) ?   current_measurement(x_pred, diodes, sᴵ, pos, alb; E_am₀ = E_am₀,
+                                             use_albedo = use_albedo, calibrate = calibrate)  :
+                         (calibrate) ? (zeros(N), zeros(N, 15 + 3 * N)) : (zeros(N), zeros(N, 6))                        
+
+    # Innovation 
+    z = [B̃ᴮ - B̃ᴮ_exp; Ĩ - I_exp]
+    H = [H_mag; H_cur]
+    Pxy = P_pred * H' 
+    Pyy = H * P_pred * H' + R
+    L = Pxy * inv(Pyy)
+
+    # Pchol_yy = qrᵣ([Pchol_pred * H'; V])
+    # K = (((Pchol_pred' * Pchol_pred * H') / Pchol_yy) / Pchol_yy')
+    x⁺, mag⁺, diodes⁺ = update(x_pred, mag, diodes, L, z; calibrate = calibrate) 
+
+    # x⁺, mag⁺, diodes⁺ = deepcopy(x_pred), deepcopy(mag), deepcopy(diodes)
+    # L, H = 0.0, 0.0
+
+    # # Kalman Gain (how much we trust the measurement over the dynamics)
+    # for _ = 1:3
+    #     # Measurement (do NOT normalize this, or at least not the current one)
+    #     B̃ᴮ_exp, H_mag = mag_measurement(x⁺, mag⁺, Bᴵ, N; calibrate = calibrate)
+
+    #     #   If in eclipse, dont use I 
+    #     I_exp,  H_cur = (norm(Ĩ) > 0.05) ?  current_measurement(x⁺, diodes⁺, sᴵ, pos, alb; E_am₀ = E_am₀,
+    #                                              use_albedo = use_albedo, calibrate = calibrate)  :
+    #                          (calibrate) ? (zeros(N), zeros(N, 15 + 3 * N)) : (zeros(N), zeros(N, 6))                        
+
+    #     # Innovation 
+    #     z = [B̃ᴮ - B̃ᴮ_exp; Ĩ - I_exp]     # maybe?
+    #     H = [H_mag; H_cur]
+
+    #     # Update Covariance 
+    #     Pxy = P_pred * H' 
+    #     Pyy = H * P_pred * H' + R
+    #     L = Pxy * inv(Pyy)
+    #     # Pchol_yy = qrᵣ([Pchol_pred * H'; V])
+    #     # L = (((Pchol_pred' * Pchol_pred * H') / Pchol_yy) / Pchol_yy')
+
+    #     @infiltrate
+    #     x⁺, mag⁺, diodes⁺ = update(x_pred, mag, diodes, L, z; calibrate = calibrate)
+    # end
+
+    # Update 
+    P_pred⁺ = P_pred - L * H * P_pred
+    Pchol⁺ = chol(P_pred⁺) 
+    # Pchol⁺_alt = qrᵣ([ Pchol_pred * (I - K * H)'; V * K']); 
 
     return x⁺, mag⁺, diodes⁺, Pchol⁺
 end
+
 
 # Prediction function that does not use FD
 """
@@ -359,14 +462,29 @@ function mag_measurement(x::SAT_STATE{T}, mag::MAGNETOMETER{T}, Bᴵ::SVector{3,
 
     ## Generate expected measurements
     Bᴮ = quat2rot(x.q)' * Bᴵ;  # Transpose to convert from inertial → body
+
+    function B(s, ζ, βₘ, Bᴮ)
+        a, b, c = s
+        ρ, λ, ν = ζ
+    
+        cal_mat = [a         0.0              0.0;
+                   b*sin(ρ)  b*cos(ρ)         0.0;
+                   c*sin(λ)  c*sin(ν)*cos(λ)  c*cos(ν)*cos(λ)]
+        B̃ᴮ = cal_mat * Bᴮ + βₘ   # What we would expect to measure
+
+        return B̃ᴮ
+    end
     
     a, b, c = mag.scale_factors 
     ρ, λ, ν = mag.non_ortho_angles 
 
-    cal_mat = [a         0.0              0.0;
-               b*sin(ρ)  b*cos(ρ)         0.0;
-               c*sin(λ)  c*sin(ν)*cos(λ)  c*cos(ν)*cos(λ)]
-    B̃ᴮ = cal_mat * Bᴮ + mag.bias   # What we would expect to measure
+    B̃ᴮ = B(mag.scale_factors, mag.non_ortho_angles, mag.bias, Bᴮ)
+
+
+    # cal_mat = [a         0.0              0.0;
+    #            b*sin(ρ)  b*cos(ρ)         0.0;
+    #            c*sin(λ)  c*sin(ν)*cos(λ)  c*cos(ν)*cos(λ)]
+    # B̃ᴮ = cal_mat * Bᴮ + mag.bias   # What we would expect to measure
 
     ## Generate Jacobian H (mag_measurement wrt state) 
     ∂ϕ = hat(Bᴮ)  
@@ -393,8 +511,18 @@ function mag_measurement(x::SAT_STATE{T}, mag::MAGNETOMETER{T}, Bᴵ::SVector{3,
         H[:, 10:12] .= ∂ζ
         H[:, 13:15] .= ∂βₘ
         # Remainder are all zero
-    end
 
+        # ## REMOVE - TESTING! (It matches)
+        # Bs(_s)  = B(_s, mag.non_ortho_angles, mag.bias, Bᴮ)
+        # Bζ(_ζ)  = B(mag.scale_factors, _ζ, mag.bias, Bᴮ)
+        # Bβ(_βₘ) = B(mag.scale_factors, mag.non_ortho_angles, _βₘ, Bᴮ)
+    
+        # ds  = ForwardDiff.jacobian(Bs, mag.scale_factors)
+        # dζ  = ForwardDiff.jacobian(Bζ, mag.non_ortho_angles)
+        # dβₘ = ForwardDiff.jacobian(Bβ, mag.bias)
+
+        # @infiltrate
+    end
 
     return B̃ᴮ, H
 end;
@@ -529,7 +657,6 @@ function update(x::SAT_STATE{T}, mag::MAGNETOMETER{T}, diodes::DIODES{N, T}, L::
 
     return x⁺, mag⁺, diodes⁺
 end
-
 
 
 
